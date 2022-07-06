6ed092073c553dfc19b79b1af08619165081dbfd
Shwe Zin <70483157+ShweZin-coder@users.noreply.github.com>
2022-07-07 02:16:25 +0630

    Delete QRcodeGeneratorandReader directory

--- QRcodeGeneratorandReader/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-let btnBack = document.querySelector('#backbtn');
-let generating = document.querySelector('#generating');
-btnBack.addEventListener('click', () => {
-    window.history.back();
-});
